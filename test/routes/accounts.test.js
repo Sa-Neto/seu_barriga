910dd78f5e8edda8158624f252b418d8666d9ead
Sa neto <sa.jrneto@gmail.com>
2022-08-16 21:52:42 -0300

    token

--- test/routes/accounts.test.js
+++ test/routes/accounts.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../../src/app');
-
+const jwt = require('jwt-simple')
 const MAIN_ROUTE = '/accounts'
 
 let user 
@@ -8,11 +8,13 @@ let user
 beforeAll(async () => {
     const res = await app.services.user.save({name:'User Account',mail:`${Date.now()}@mail.com`,passwd:'123456'})
     user = {...res[0]};
+    user.token = jwt.encode(user,'Segredo!')
 });
 
 test('Deve inserir um const com sucesso',() => {
     return request(app).post(MAIN_ROUTE)
         .send({name:'Acc #1',user_id:user.id})
+        .set('authorization',`bearer ${user.token}`)
         .then((result) => {
             expect(result.status).toBe(201)
             expect(result.body.name).toBe('Acc #1');
@@ -22,6 +24,7 @@ test('Deve inserir um const com sucesso',() => {
 test('Não deve inserir uma conta sem nome',() => {
     return request(app).post(MAIN_ROUTE)
         .send({user_id:user.id})
+        .set('authorization',`bearer ${user.token}`)
         .then((result) => {
             expect(result.status).toBe(400)
             expect(result.body.error).toBe('Nome é um atributo obrigatório')
@@ -31,7 +34,7 @@ test('Não deve inserir uma conta sem nome',() => {
 test('Deve listar todoas as contas',() => {
     return app.db('accounts')
         .insert({name:'Acc list', user_id:user.id})
-        .then(() => request(app).get(MAIN_ROUTE))
+        .then(() => request(app).get(MAIN_ROUTE).set('authorization',`bearer ${user.token}`))
         .then((res) => {
             expect(res.status).toBe(200);
             expect(res.body.length).toBeGreaterThan(0)
@@ -41,7 +44,7 @@ test('Deve listar todoas as contas',() => {
 test('Deve retornar uma conta por Id' ,() => {
     return app.db('accounts')
         .insert({name:'Acc By Id',user_id:user.id} ,['id'])
-        .then(acc => request(app).get(`${MAIN_ROUTE}/${acc[0].id}`))
+        .then(acc => request(app).get(`${MAIN_ROUTE}/${acc[0].id}`).set('authorization',`bearer ${user.token}`))
         .then((res) => {
             expect(res.status).toBe(200);
             expect(res.body.name).toBe('Acc By Id');
@@ -53,7 +56,9 @@ test('Deve alterar uma conta',() => {
     return app.db('accounts')
         .insert({name:'Acc to Upgrate', user_id:user.id},['id'])
         .then(acc => request(app).put(`${MAIN_ROUTE}/${acc[0].id}`)
-            .send({name:'Acc Updated'}))
+            .send({name:'Acc Updated'})
+            .set('authorization',`bearer ${user.token}`)
+            )
         .then((res) => {
             expect(res.status).toBe(200);
             expect(res.body.name).toBe('Acc Updated')
@@ -62,7 +67,8 @@ test('Deve alterar uma conta',() => {
 test('Deve remover uma conta',() => {
     return app.db('accounts')
         .insert({name:'Acc to remove',user_id:user.id},['id'])
-        .then(acc => request(app).delete(`${MAIN_ROUTE}/${acc[0].id}`))
+        .then(acc => request(app).delete(`${MAIN_ROUTE}/${acc[0].id}`)
+        .set('authorization',`bearer ${user.token}`))
         .then((res) => {
             expect(res.status).toBe(204)
         })
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest')
-
+const jwt = require('jwt-simple')
 const app = require('../../src/app');
 const mail = `${Date.now()}@mail.com`
+
+let user 
+
+beforeAll(async () => {
+    const res = await app.services.user.save({name:'User Account',mail:`${Date.now()}@mail.com`,passwd:'123456'})
+    user = {...res[0]};
+    user.token = jwt.encode(user,'Segredo!')
+});
+
 test('Deve listar todos os usuários',() => {
     return request(app).get('/users')
+    .set('authorization',`bearer ${user.token}`)
     .then((res) => {
         expect(res.status).toBe(200)
         expect(res.body.length).toBeGreaterThan(0);
@@ -13,6 +23,7 @@ test('Deve listar todos os usuários',() => {
 test('Deve inserir usuário com sucesso',() => {
     return request(app).post('/users')
     .send({name:'Walter Mitty',mail,passwd:'123456'})
+    .set('authorization',`bearer ${user.token}`)
     .then((res) => {
         expect(res.status).toBe(201)
         expect(res.body.name).toBe('Walter Mitty')
@@ -23,6 +34,7 @@ test('Deve inserir usuário com sucesso',() => {
 test('Deve armazenar senha criptografada',async() => {
     const res = await request(app).post('/users')
     .send({name:"Walter Mitty",mail:`${Date.now()}@mail.com`,passwd:"123456"})
+    .set('authorization',`bearer ${user.token}`)  
         expect(res.status).toBe(201)
         const {id} = res.body
         const userDB = await app.services.user.findOne({id});
@@ -33,6 +45,7 @@ test('Deve armazenar senha criptografada',async() => {
 test('Não deve inserir usuário sem nome', () => {
     return request(app).post('/users')
         .send({mail:'walter@mail.com' , passwd:'123456'})
+        .set('authorization',`bearer ${user.token}`)
         .then((res) => {
             expect(res.status).toBe(400)
             expect(res.body.error).toBe('Nome é um atributo obrigatório')
@@ -42,6 +55,7 @@ test('Não deve inserir usuário sem nome', () => {
 test('Não deve inserir usuário sem email',async() => {
     const result = await request(app).post('/users')
         .send({name:'Walter Mitty',passwd:'123456'})
+        .set('authorization',`bearer ${user.token}`)
         .then((res) => {
             expect(res.status).toBe(400);
             expect(res.body.error).toBe('Email é um atributo obrigatório')
@@ -51,6 +65,7 @@ test('Não deve inserir usuário sem email',async() => {
 test('Não deve inserir usuário sem senha', (done) => {
     request(app).post('/users')
     .send({name:'Walter Mitty',mail:'walter@mail.com'})
+    .set('authorization',`bearer ${user.token}`)
     .then((res) => {
         expect(res.status).toBe(400)
         expect(res.body.error).toBe('Senha é um atributo obrigatório');
@@ -61,6 +76,7 @@ test('Não deve inserir usuário sem senha', (done) => {
 test('Não deve inserir usuário com email existente',() => {
     return request(app).post('/users')
         .send({name:'Walter Mitty',mail,passwd:'123456'})
+        .set('authorization',`bearer ${user.token}`)
         .then((res) => {
             expect(res.status).toBe(400)
             expect(res.body.error).toBe('Já existe um usuário com esse email')
